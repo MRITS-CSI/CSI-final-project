a7aad3f42a2dee471f08db494e8ae522e7476f7c
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2021-12-21 01:09:24 +0530

    Final Changes

--- src/Utils/Objects.js
+++ src/Utils/Objects.js
@@ -3,10 +3,25 @@ export const Objects = ['potted plant'];
 
 export const QuestionObject = async () => {
 	try {
+		if (
+			window.localStorage.getItem('rid') &&
+			window.localStorage.getItem('ans')
+		) {
+			return {
+				riddle: window.localStorage.getItem('rid'),
+				answer: window.localStorage.getItem('ans'),
+			};
+		}
 		//	let token = window.sessionStorage.getItem('jwt');
 		let { data } = await axios.get(
 			'https://csimrits.tech/api/v1/riddle/randomRiddle'
 		);
+		window.localStorage.setItem('rid', data.riddle);
+		window.localStorage.setItem('ans', data.answer);
+
+		await axios.patch(`https://csimrits.tech/api/v1/riddle/${data._id}`, {
+			picked: true,
+		});
 		return data;
 	} catch (err) {
 		alert(err.message);
