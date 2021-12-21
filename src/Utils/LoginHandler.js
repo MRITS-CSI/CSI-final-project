42b08107d4671bcdd1f6e1b60c64e405f524fcbc
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2021-12-21 22:08:30 +0530

    Changed URLS

--- src/Utils/LoginHandler.js
+++ src/Utils/LoginHandler.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 /**
  *
@@ -6,34 +6,34 @@ import axios from "axios";
  * @param {String} password
  */
 export const checkLogin = async (username, password) => {
-  try {
-    let { data } = await axios.post("http://localhost:8000/api/v1/login", {
-      username,
-      password,
-    });
-    if (data.status === "Failed")
-      alert("Login Failed Please check your credentials");
-    if (data.status === "ok" && data.user.secondRound === true) {
-      window.sessionStorage.setItem("jwt", data.token);
-      window.location.pathname = "/AI";
-    } else {
-      alert("Login Failed !!! Please contact CSI Team");
-    }
-  } catch (e) {
-    console.log(e.message);
-    alert(e.message);
-  }
+	try {
+		let { data } = await axios.post('https://csimrits.tech/api/v1/login', {
+			username,
+			password,
+		});
+		if (data.status === 'Failed')
+			alert('Login Failed Please check your credentials');
+		if (data.status === 'ok' && data.user.secondRound === true) {
+			window.sessionStorage.setItem('jwt', data.token);
+			window.location.pathname = '/AI';
+		} else {
+			alert('Login Failed !!! Please contact CSI Team');
+		}
+	} catch (e) {
+		console.log(e.message);
+		alert(e.message);
+	}
 };
 
 export const checkToken = async (token) => {
-  //	console.log(token);
-  if (!token) return false;
+	//	console.log(token);
+	if (!token) return false;
 
-  let { data } = await axios.post("http://localhost:8000/api/v1/login/check", {
-    jwt: token,
-  });
+	let { data } = await axios.post('https://csimrits.tech/api/v1/login/check', {
+		jwt: token,
+	});
 
-  if (data.status === "verified") return true;
+	if (data.status === 'verified') return true;
 
-  return false;
+	return false;
 };
